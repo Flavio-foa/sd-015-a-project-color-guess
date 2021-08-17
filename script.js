4855a9874ae309b14d07889000ad65e0b2210387
Luan Vitor <luan_vittor@hotmail.com>
2021-08-17 17:47:11 -0300

    All done

--- script.js
+++ script.js
@@ -9,43 +9,46 @@ function randomNumber() {
   const g = parseInt(Math.random() * 255, 10);
   const b = parseInt(Math.random() * 255, 10);
   const rgb = `(${r}, ${g}, ${b})`;
-  p.innerText = rgb;
-  addColor();
-  function addColor() {
-    for (let i = 0; i < balls.length; i += 1) {
-      const r = parseInt(Math.random() * 255, 10);
-      const g = parseInt(Math.random() * 255, 10);
-      const b = parseInt(Math.random() * 255, 10);
-      balls[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    }
-    const oneToSIx = parseInt(Math.random() * 6, 10);
-    balls[oneToSIx].style.backgroundColor = 'rgb' + rgb;
-  }
-  const resposta = document.getElementById('answer');
-  const cores = document.querySelectorAll('.ball');
-
-  function click(event) {
-    if (event.target.style.backgroundColor === 'rgb' + rgb) {
-      resposta.innerText = 'Acertou!';
-      placar();
-    } else if (event.target.style.backgroundColor !== 'rgb' + rgb) {
-      resposta.innerText = 'Errou! Tente novamente!';
-    }
-  }
-  for (let i = 0; i < cores.length; i += 1) {
-    cores[i].addEventListener('click', click);
-  }
+  return rgb;
 }
+let rgb2 = randomNumber();
+p.innerText = rgb2;
 
-function resetGame() {
-  h1.innerText = 'Escolha uma cor';
-  randomNumber();
+function addColor(rgb3) {
+  for (let i = 0; i < balls.length; i += 1) {
+    const newrgb = randomNumber();
+    const aux = `rgb${newrgb}`;
+    balls[i].style.backgroundColor = aux;
+  }
+  const oneToSIx = parseInt(Math.random() * 6, 10);
+  balls[oneToSIx].style.backgroundColor = `rgb${rgb3}`;
 }
+addColor(rgb2);
 
-botaoReset.addEventListener('click', resetGame);
-
-window.onload = randomNumber();
+const resposta = document.getElementById('answer');
+const cores = document.querySelectorAll('.ball');
 
 function placar() {
   score.innerText = parseInt(score.innerText, 10) + 3;
 }
+
+function click(event) {
+  if (event.target.style.backgroundColor === `rgb${rgb2}`) {
+    resposta.innerText = 'Acertou!';
+    placar();
+  } else if (event.target.style.backgroundColor !== `rgb${rgb2}`) {
+    resposta.innerText = 'Errou! Tente novamente!';
+  }
+}
+for (let i = 0; i < cores.length; i += 1) {
+  cores[i].addEventListener('click', click);
+}
+
+function resetGame() {
+  h1.innerText = 'Escolha uma cor';
+  rgb2 = randomNumber();
+  addColor(rgb2);
+  p.innerText = rgb2;
+}
+
+botaoReset.addEventListener('click', resetGame);
