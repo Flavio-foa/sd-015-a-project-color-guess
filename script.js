5128575cd9e7f4707888267b89ba4abc7e1be83b
Guarize <pedroguarize@hotmail.com>
2021-08-17 18:56:21 -0300

    Requirement - 7

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const colorsBalls = document.querySelectorAll('.ball');
 const colorsContainer = document.getElementById('colors-container');
 const answer = document.getElementById('answer');
 const resetGameBtn = document.getElementById('reset-game');
+const score = document.getElementById('score');
 
 // Generates random RGB color
 function generateRandomRGB() {
@@ -34,21 +35,6 @@ window.onload = function setsColors() {
   return colorsBalls[randomIndex];
 };
 
-// Checks if clicked color is correct
-function game(event) {
-  const clickedColor = event.target;
-  if (
-    clickedColor.style.backgroundColor === rgbTarget.innerText.toLowerCase()
-  ) {
-    answer.innerText = 'Acertou!';
-    return true;
-  }
-  answer.innerText = 'Errou! Tente novamente!';
-  return false;
-}
-
-colorsContainer.addEventListener('click', game);
-
 // Reset game
 function reset() {
   const newColors = randomColorArray();
@@ -63,3 +49,20 @@ function reset() {
 }
 
 resetGameBtn.addEventListener('click', reset);
+
+// Checks if clicked color is correct
+function game(event) {
+  const clickedColor = event.target;
+  if (
+    clickedColor.style.backgroundColor === rgbTarget.innerText.toLowerCase()
+  ) {
+    score.innerHTML = Number(score.innerHTML) + 3;
+    answer.innerText = 'Acertou!';
+    setTimeout(reset, 1500);
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+    setTimeout(reset, 1500);
+  }
+}
+
+colorsContainer.addEventListener('click', game);
