a5d158bb742933a48788d27c3c346b0135dee877
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-16 21:32:36 -0300

    Requisito 6 feito

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const rgbTxt = document.getElementById('rgb-color');
 const balls = document.querySelectorAll('.ball');
 const instructions = document.getElementById('answer');
+const resetButton = document.getElementById('reset-game');
 
 function randomColor() {
   const red = Math.floor(Math.random() * 255);
@@ -15,7 +16,7 @@ function selectRight() {
   return Math.floor(Math.random() * 6);
 }
 
-const rigthBall = selectRight();
+let rigthBall = selectRight();
 
 for (let i = 0; i < balls.length; i += 1) {
   if (i !== rigthBall) {
@@ -33,3 +34,16 @@ for (let i = 0; i < balls.length; i += 1) {
     }
   });
 }
+
+resetButton.addEventListener('click', () => {
+  rgbTxt.innerText = randomColor();
+  rigthBall = selectRight();
+  instructions.innerText = 'Escolha uma cor';
+  for (let i = 0; i < balls.length; i += 1) {
+    if (i !== rigthBall) {
+      balls[i].style.backgroundColor = `rgb${randomColor()}`;
+    } else {
+      balls[i].style.backgroundColor = `rgb${rgbTxt.innerText}`;
+    }
+  }
+});
