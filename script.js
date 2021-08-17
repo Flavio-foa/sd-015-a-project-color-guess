6dac262e5dbb1750d96cb0a9835c2ea742caa288
Braien <braienmp@outlook.com>
2021-08-17 19:26:47 -0300

    adiciona opçoes de cores de forma dinamica

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const paragraph = document.getElementById('rgb-color');
+paragraph.innerHTML = '(168, 34, 1)';
 
+function generateRGBColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+document.querySelector('.ball').style.backgroundColor = 'black'
+
+window.onload = function generateColors() {
+  const balls = document.querySelectorAll('.ball');
+  balls.forEach(element => {
+    element.style.backgroundColor = generateRGBColor();
+  });
+};
