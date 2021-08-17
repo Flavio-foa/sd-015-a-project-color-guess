0f41d3057f9a0b7930107a00f4e2d1de462df8b8
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-16 21:25:51 -0300

    Requisito 5 feito

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const rgbTxt = document.getElementById('rgb-color');
 const balls = document.querySelectorAll('.ball');
+const instructions = document.getElementById('answer');
 
 function randomColor() {
   const red = Math.floor(Math.random() * 255);
@@ -10,6 +11,25 @@ function randomColor() {
 
 rgbTxt.innerText = randomColor();
 
+function selectRight() {
+  return Math.floor(Math.random() * 6);
+}
+
+const rigthBall = selectRight();
+
 for (let i = 0; i < balls.length; i += 1) {
-  balls[i].style.backgroundColor = `rgb${randomColor()}`;
+  if (i !== rigthBall) {
+    balls[i].style.backgroundColor = `rgb${randomColor()}`;
+  } else {
+    balls[i].style.backgroundColor = `rgb${rgbTxt.innerText}`;
+  }
+  balls[i].addEventListener('click', (ball) => {
+    const rightColor = `rgb${rgbTxt.innerText}`;
+    const clickedBall = ball.target.style.backgroundColor;
+    if (rightColor === clickedBall) {
+      instructions.innerText = 'Acertou!';
+    } else {
+      instructions.innerText = 'Errou! Tente novamente!';
+    }
+  });
 }
