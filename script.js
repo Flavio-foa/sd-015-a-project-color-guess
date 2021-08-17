029a6c3379f29b0796fc1bb5c530958f9bbf1126
Luan Vitor <luan_vittor@hotmail.com>
2021-08-16 23:17:25 -0300

    All done

--- script.js
+++ script.js
@@ -1,70 +1,51 @@
+const balls = document.querySelectorAll('.ball');
+const p = document.getElementById('rgb-color');
+const h1 = document.querySelector('h1');
+const botaoReset = document.getElementById('reset-game');
+const score = document.getElementById('score');
 
 function randomNumber() {
-  let r = parseInt(Math.random() * 255);
-  let g = parseInt(Math.random() * 255);
-  let b = parseInt(Math.random() * 255);
-  let rgb = `(${r}, ${g}, ${b})`
-  p.innerText = rgb
-  addColor()
+  const r = parseInt(Math.random() * 255, 10);
+  const g = parseInt(Math.random() * 255, 10);
+  const b = parseInt(Math.random() * 255, 10);
+  const rgb = `(${r}, ${g}, ${b})`;
+  p.innerText = rgb;
+  addColor();
   function addColor() {
-    for (let elemetos of balls) {
-      let r = parseInt(Math.random() * 255);
-      let g = parseInt(Math.random() * 255);
-      let b = parseInt(Math.random() * 255);
-      elemetos.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    for (let i = 0; i < balls.length; i += 1) {
+      const r = parseInt(Math.random() * 255, 10);
+      const g = parseInt(Math.random() * 255, 10);
+      const b = parseInt(Math.random() * 255, 10);
+      balls[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     }
-    let oneToSIx = parseInt(Math.random() * 6);
-    balls[oneToSIx].style.backgroundColor = "rgb" + rgb
+    const oneToSIx = parseInt(Math.random() * 6, 10);
+    balls[oneToSIx].style.backgroundColor = 'rgb' + rgb;
   }
-  let resposta = document.getElementById('answer')
-  let cores = document.querySelectorAll('.ball')
-  for (elemetos of cores) {
-    elemetos.addEventListener('click', click);
-  }
+  const resposta = document.getElementById('answer');
+  const cores = document.querySelectorAll('.ball');
+
   function click(event) {
-    if (event.target.style.backgroundColor === "rgb" + rgb) {
-      resposta.innerText = 'Acertou!'
-      placar()
-    } else if (event.target.style.backgroundColor !== "rgb" + rgb) {
-      resposta.innerText = 'Errou! Tente novamente!'
+    if (event.target.style.backgroundColor === 'rgb' + rgb) {
+      resposta.innerText = 'Acertou!';
+      placar();
+    } else if (event.target.style.backgroundColor !== 'rgb' + rgb) {
+      resposta.innerText = 'Errou! Tente novamente!';
     }
+  }
+  for (let i = 0; i < cores.length; i += 1) {
+    cores[i].addEventListener('click', click);
   }
 }
-
-
-
-let balls = document.querySelectorAll('.ball')
-let p = document.getElementById('rgb-color')
-window.onload = randomNumber()
-
-
-// function click(event) {
-//   if (event.target.style.backgroundColor === "rgb" + rgb ) {
-//     resposta.innerText = 'Acertou!'
-//     placar()
-//   } else if (event.target.style.backgroundColor !== rgbCompleted) {
-//     resposta.innerText = 'Errou! Tente novamente!' 
-//   }
-// }
-
-// let resposta = document.getElementById('answer')
-// let cores = document.querySelectorAll('.ball')
-// for (elemetos of cores) {
-//   elemetos.addEventListener('click', click);
-// }
 
 function resetGame() {
-  h1.innerText = 'Escolha uma cor'
-  randomNumber()
+  h1.innerText = 'Escolha uma cor';
+  randomNumber();
 }
 
-let h1 = document.querySelector('h1')
-let botaoReset = document.getElementById('reset-game')
-botaoReset.addEventListener('click', resetGame)
+botaoReset.addEventListener('click', resetGame);
+
+window.onload = randomNumber();
 
 function placar() {
-  score.innerText = parseInt(score.innerText) + 3
-
+  score.innerText = parseInt(score.innerText, 10) + 3;
 }
-
-let score = document.getElementById('score');
