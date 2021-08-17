7f662cd74f59646f03861fa1b637af7b839a9746
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-16 21:10:34 -0300

    add score counter

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let ISRGBHASH = false;
+let count;
 
 function generateRandomColor() {
   const rgbColor = [];
@@ -55,10 +56,13 @@ function createOptionColors() {
 function createParagraphText() {
   const newParagraph = document.createElement('p');
 
-  newParagraph.innerHTML = 'Escolha uma cor';
   newParagraph.id = 'answer';
-
+  newParagraph.innerHTML = 'Escolha uma cor';
   document.querySelector('main').appendChild(newParagraph);
+}
+
+function saveScore() {
+  localStorage.setItem('counter', count);
 }
 
 function validateGuess() {
@@ -69,6 +73,9 @@ function validateGuess() {
 
     if (guessBackground === `rgb(${rgbHash[0]}, ${rgbHash[1]}, ${rgbHash[2]})`) {
       document.getElementById('answer').innerHTML = 'Acertou!';
+      count += Number(3);
+      saveScore();
+      document.getElementById('score').innerHTML = `Placar: ${count}`;
     } else {
       document.getElementById('answer').innerHTML = 'Errou! Tente novamente!';
     }
@@ -78,14 +85,32 @@ function validateGuess() {
 function createStartBtn() {
   const startBtn = document.createElement('button');
   startBtn.innerHTML = 'Iniciar/Resetar';
+  startBtn.id = 'reset-game';
   document.querySelector('main').appendChild(startBtn);
 
   startBtn.addEventListener('click', () => {
-    location.reload();
+    window.location.reload();
   });
 }
 
+function createScore() {
+  const newCount = document.createElement('p');
+  console.log(localStorage.getItem('counter'));
+  if (localStorage.getItem('counter') === null) {
+    count = Number(0);
+  } else {
+    count = Number(localStorage.getItem('counter'));
+  }
+  console.log(count);
+  console.log(localStorage.getItem('counter'));
+
+  newCount.innerHTML = `Placar: ${count}`;
+  newCount.id = 'score';
+  document.querySelector('main').appendChild(newCount);
+}
+
 createOptionColors();
+createScore();
 createParagraphText();
 validateGuess();
 createStartBtn();
