d713c297ad5ec75bbbf4d8fc4e423ad993b200a7
lucas-morais <lucasmoraisss@gmail.com>
2021-08-18 11:30:56 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+function generateRandomInteger(max) {
+  return Math.round(Math.random() * max);
+}
+
 function generateColor() {
   const values = [];
   for (let index = 0; index < 3; index += 1) {
-    values[index] = Math.round(Math.random() * 255);
+    values[index] = generateRandomInteger(255);
   }
   return `(${values[0]}, ${values[1]}, ${values[2]})`;
 }
 
-function colorToGuess() {
-  const rgbColor = document.getElementById('rgb-color');
-  rgbColor.innerHTML = generateColor();
+function generateRandomColors() {
+  const balls = document.getElementsByClassName('ball');
+  const colorRgb = document.getElementById('rgb-color');
+  const correct = generateRandomInteger(balls.length - 1);
+  for (let index = 0; index < balls.length; index += 1) {
+    const color = generateColor();
+    balls[index].style.backgroundColor = `rgb${color}`;
+    if (index === correct) {
+      colorRgb.innerHTML = color;
+    }
+  }
 }
 
-colorToGuess();
+generateRandomColors();
