3ae31ccd15596b1e9a924803cb91bd24f6ffbe03
Thiago <thiago_ssfr@hotmail.com>
2021-08-19 08:39:55 -0300

    Requisito 3 e 4

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const paragrafo = document.createElement('p');
+const paragrafo = document.createElement('div');
 const rgbPrincipal = document.getElementById('rgb-principal');
+const number = 6;
+const container = document.querySelector('#main-container');
+
 function gerarCor() {
   const r = Math.round(Math.random() * 255);
   const g = Math.round(Math.random() * 255);
@@ -12,3 +15,18 @@ paragrafo.innerHTML = gerarCor();
 paragrafo.id = 'rgb-color';
 
 rgbPrincipal.appendChild(paragrafo);
+
+for(let index = 0; index < number; index += 1){
+  const circuitos = document.createElement('div');
+  const cor = gerarCor();
+  circuitos.className = 'ball';
+  circuitos.innerHTML = cor;
+  circuitos.style.backgroundColor = cor;
+  circuitos.style.width = '50px'
+  circuitos.style.height = '50px';
+  circuitos.style.display = 'inline-block';
+  circuitos.style.padding = '10px';
+  circuitos.style.margin = '10px';
+  circuitos.style.borderRadius = '50px';
+  container.appendChild(circuitos);
+}
