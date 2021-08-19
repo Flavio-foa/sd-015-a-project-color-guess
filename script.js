7980b257274352f922c9dba9aada920876eb4b33
Thiago <thiago_ssfr@hotmail.com>
2021-08-19 09:44:35 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const paragrafo = document.createElement('div');
 const rgbPrincipal = document.getElementById('rgb-principal');
 const number = 6;
-const container = document.querySelector('#main-container');
+const rgbContainer = document.querySelector('#rgb-container');
+const resposta = document.getElementById('answer');
 
 function gerarCor() {
   const r = Math.round(Math.random() * 255);
@@ -11,22 +12,55 @@ function gerarCor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-paragrafo.innerHTML = gerarCor();
-paragrafo.id = 'rgb-color';
+function corPrincipal(cor) {
+  paragrafo.innerHTML = cor;
+  paragrafo.id = 'rgb-color';
 
-rgbPrincipal.appendChild(paragrafo);
-
-for(let index = 0; index < number; index += 1){
-  const circuitos = document.createElement('div');
-  const cor = gerarCor();
-  circuitos.className = 'ball';
-  circuitos.innerHTML = cor;
-  circuitos.style.backgroundColor = cor;
-  circuitos.style.width = '50px'
-  circuitos.style.height = '50px';
-  circuitos.style.display = 'inline-block';
-  circuitos.style.padding = '10px';
-  circuitos.style.margin = '10px';
-  circuitos.style.borderRadius = '50px';
-  container.appendChild(circuitos);
+  rgbPrincipal.appendChild(paragrafo);
 }
+
+function coresAleatoria() {
+  for (let index = 0; index < number; index += 1) {
+    const circuitos = document.createElement('div');
+    const cor = gerarCor();
+    circuitos.className = 'ball';
+    circuitos.innerHTML = cor;
+    circuitos.style.backgroundColor = cor;
+    circuitos.style.width = '50px';
+    circuitos.style.height = '50px';
+    circuitos.style.display = 'inline-block';
+    circuitos.style.padding = '10px';
+    circuitos.style.margin = '10px';
+    circuitos.style.borderRadius = '50px';
+    rgbContainer.appendChild(circuitos);
+  }
+  const ball = document.querySelectorAll('.ball');
+  const random = Math.ceil(Math.random() * 6) - 1;
+  console.log(ball[random].style.backgroundColor);
+  corPrincipal(ball[random].style.backgroundColor);
+}
+
+coresAleatoria();
+
+function comparaCor(cor) {
+  if (paragrafo.innerHTML === cor) {
+    return true;
+  }
+  return false;
+}
+
+function respostaFunction(result) {
+  if (result) {
+    resposta.innerHTML = 'Acertou!';
+    return;
+  }
+  resposta.innerHTML = 'Errou! Tente novamente!';
+}
+
+resposta.innerHTML = 'Escolha uma cor';
+
+rgbContainer.addEventListener('click', (event) => {
+  const cor = event.target.style.backgroundColor;
+  const result = comparaCor(cor);
+  respostaFunction(result);
+});
