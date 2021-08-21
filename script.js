04cba2ac991f65638d7df6e1f60e389af14da1fe
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-21 19:39:14 -0400

    Resolve verificaçao " Será verificado se o circulo colorido for o correto, deve ser exibido o texto "Acertou!"; " do requisito 5 projeto guess-color bloco 5 - 2º projeto bonus

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const ball = '.ball';
-const rgbColor = 'rgb-color';
+const rgbColor = document.getElementById('rgb-color');
 const answer = 'answer';
 
 function randomBgC() {
     const getBalls = document.querySelectorAll(ball);
+    const aleatoryNumber = getAleatoryColor();
     for (let i = 0; i < getBalls.length; i += 1) {
         getBalls[i].addEventListener('click', answerColor);
         let randomR = Math.round(Math.random() * 255);
         let randomG = Math.round(Math.random() * 255);
         let randomB = Math.round(Math.random() * 255);
         getBalls[i].style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        if (i === aleatoryNumber) {
+            let color = getBalls[i].style.backgroundColor;
+            rgbColor.style.color = color;
+            rgbColor.innerHTML = color.slice(3);
+        }
     }
+}
+
+function getAleatoryColor() { // Retorna um número aleatorio
+    let aleatoryNumber = Math.floor(Math.random() * 6);
+    return aleatoryNumber;
 }
 
 function answerColor (event) {
     const clicked = event.target;
     const color = clicked.style.backgroundColor;
-    const getRgb = document.getElementById(rgbColor);
-    const rgbText = `rgb${getRgb.innerHTML}`;
+    const colorSlice = color.slice(3);
+    const rgbColortext = rgbColor.innerHTML;
     const getAnswerP = document.getElementById(answer);
-    console.log(color);
-    console.log(rgbText);
-    if (color === rgbText) {
-        console.log('color= '+typeof(color));
-        console.log('rgbText= '+typeof(rgbText));
+    if (colorSlice === rgbColortext) {
+        // console.log('color= '+typeof(color));
+        // console.log('rgbText= '+typeof(rgbText));
         getAnswerP.innerHTML = 'Acertou!';
     } else {
         getAnswerP.innerHTML = 'Errou! Tente novamente!';
