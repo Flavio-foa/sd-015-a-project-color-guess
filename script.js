4d385cc690470db15b90b94e79b7fd6463fdbfe7
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-22 16:00:35 -0400

    Resolve eslint let to const, algumas identaçoes e segundo parametro do parseint()

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ function answerColor(event) {
   if (colorSlice === rgbColortext) {
     getAnswerP.innerHTML = 'Acertou!';
     let scoreContent = getScore.innerText;
-    let splitScore = scoreContent.split(' ');
-    splitScore[1] = parseInt(splitScore[1]);
+    const splitScore = scoreContent.split(' ');
+    splitScore[1] = parseInt(splitScore[1], 10);
     splitScore[1] += 3;
     scoreContent += splitScore[1];
-    let backToText = splitScore.join(' ');
+    const backToText = splitScore.join(' ');
     getScore.innerText = backToText;
-    console.log(getScore.innerText)
+    console.log(getScore.innerText);
   } else {
     getAnswerP.innerHTML = 'Errou! Tente novamente!';
   }
@@ -35,15 +35,15 @@ function randomBgC() {
   const aleatoryNumber = getAleatoryColor();
   for (let i = 0; i < getBalls.length; i += 1) {
     getBalls[i].addEventListener('click', answerColor);
-    let randomR = Math.round(Math.random() * 255);
-    let randomG = Math.round(Math.random() * 255);
-    let randomB = Math.round(Math.random() * 255);
+    const randomR = Math.round(Math.random() * 255);
+    const randomG = Math.round(Math.random() * 255);
+    const randomB = Math.round(Math.random() * 255);
     getBalls[i].style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
     if (i === aleatoryNumber) {
-        let color = getBalls[i].style.backgroundColor;
-        rgbColor.style.color = color;
-        rgbColor.innerHTML = color.slice(3);
-      }
+      const color = getBalls[i].style.backgroundColor;
+      rgbColor.style.color = color;
+      rgbColor.innerHTML = color.slice(3);
+    }
   }
   const getAnswer = document.getElementById(answer);
   getAnswer.innerText = 'Escolha uma cor';
@@ -60,4 +60,4 @@ getResetButton.addEventListener('click', setResetButton);
 
 window.onload = function () {
   randomBgC();
-}
+};
