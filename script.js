087a1116a9fe8ae75ee8e47dfae56afea98d2d0b
Braien <braienmp@outlook.com>
2021-09-11 11:11:45 -0300

    implementa placar para contabilizar acertos no jogo

--- script.js
+++ script.js
@@ -9,40 +9,40 @@ function generateRGBColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-function showMessage(event) {
+const balls = document.querySelectorAll('.ball');
+
+function checkColor(event) {
   const color = event.target;
+  const scoreParagraph = document.getElementById('score');
+  let score = parseInt(scoreParagraph.innerText.match(/\d+/).join(), 10);
   if (color.id === 'correct') {
-    message.innerText = 'Acertou!';
+    message.innerText = 'Acertou! Jogue novamente.';
+    score += 3;
+    scoreParagraph.innerText = `Pontuação: ${score}`;
+    color.removeAttribute('id');
+    balls.forEach((ball) => ball.removeEventListener('click', checkColor));
   } else {
     message.innerText = 'Errou! Tente novamente!';
   }
 }
 
-const balls = document.querySelectorAll('.ball');
-
-window.onload = function init() {
+function init() {
   balls.forEach((element) => {
     const array = element;
     array.style.backgroundColor = generateRGBColor();
-    array.addEventListener('click', showMessage);
+    array.addEventListener('click', checkColor);
   });
   const correctBall = balls[Math.floor(Math.random() * 6)];
   correctBall.id = 'correct';
   const correctColor = correctBall.style.backgroundColor;
   const arrayRGB = correctColor.match(/\d+/g);
   paragraph.innerText = `(${arrayRGB[0]}, ${arrayRGB[1]}, ${arrayRGB[2]})`;
-};
+}
+
+window.onload = () => init();
 
 function reset() {
-  balls.forEach((element) => {
-    const array = element;
-    array.style.backgroundColor = generateRGBColor();
-  });
-  const correctBall = balls[Math.floor(Math.random() * 6)];
-  correctBall.id = 'correct';
-  const correctColor = correctBall.style.backgroundColor;
-  const arrayRGB = correctColor.match(/\d+/g);
-  paragraph.innerText = `(${arrayRGB[0]}, ${arrayRGB[1]}, ${arrayRGB[2]})`;
+  init();
   message.innerHTML = 'Escolha uma cor';
 }
 
