8c83da9b406ec60324c3f7b2fa47ea605c88a616
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-30 16:52:33 -0300

    finaliza projeto

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 function randomNumber(max) {
-  return Math.floor(Math.random() * (max + 1))
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function randomColor() {
-  return `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`
+  return `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`;
+}
+
+function checkAnswer({ target }) {
+  resetSelected();
+  const score = document.querySelector('#score');
+  const instruction = document.getElementById('answer');
+  target.classList.add('selected');
+  if (target.id === 'correct') {
+    instruction.innerText = 'Acertou! Novas cores!';
+    placar += 3;
+  } else {
+    instruction.innerText = 'Errou! Tente novamente';
+    placar -= 1;
+  }
+  score.innerText = `Placar: ${placar}`;
 }
 
 function generateColors() {
+  const correct = document.querySelector('#correct');
+  if (correct) { correct.id = ''; }
   const instruction = document.getElementById('answer');
-  instruction.innerText = 'Escolha uma cor'
+  instruction.innerText = 'Escolha uma cor';
+
   const getBalls = document.getElementsByClassName('ball');
-  const getSpan = document.getElementById('rgb-color');
   const balls = Array.from(getBalls);
 
   balls[randomNumber(5)].id = 'correct';
+}
 
+function chooseCorrect() {
+  const getSpan = document.getElementById('rgb-color');
+  const getBalls = document.getElementsByClassName('ball');
+  const balls = Array.from(getBalls);
   const correct = document.getElementById('correct');
 
-  balls.forEach(ball => {
+  balls.forEach((ball) => {
     ball.style.backgroundColor = randomColor();
     ball.addEventListener('click', checkAnswer);
   });
@@ -26,14 +48,17 @@ function generateColors() {
 }
 
 generateColors();
+chooseCorrect();
+let placar = 0;
 
-function checkAnswer({ target }) {
-  const selected = document.querySelector('.selected')
-  if (selected) { selected.classList.remove('selected')}
-  const instruction = document.getElementById('answer');
-  target.classList.add('selected');
-  (target.id === 'correct') ? instruction.innerText = 'Acertou! Novas cores!' : instruction.innerText = 'Errou! Tente novamente'
+function resetSelected() {
+  const selected = document.querySelector('.selected');
+  if (selected) { selected.classList.remove('selected'); }
 }
 
 const reset = document.getElementById('reset-game');
-reset.addEventListener('click', generateColors);
+reset.addEventListener('click', () => {
+  resetSelected();
+  generateColors();
+  chooseCorrect();
+});
