6793f53226e4900fe77853e8a987812c20e7bfa3
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-30 16:13:49 -0300

    requisitos 5 e 6

--- script.js
+++ script.js
@@ -7,26 +7,33 @@ function randomColor() {
 }
 
 function generateColors() {
+  const instruction = document.getElementById('answer');
+  instruction.innerText = 'Escolha uma cor'
   const getBalls = document.getElementsByClassName('ball');
   const getSpan = document.getElementById('rgb-color');
   const balls = Array.from(getBalls);
 
-  balls[randomNumber(5)].id = 'answer';
+  balls[randomNumber(5)].id = 'correct';
 
-  const answer = document.getElementById('answer');
+  const correct = document.getElementById('correct');
 
   balls.forEach(ball => {
     ball.style.backgroundColor = randomColor();
     ball.addEventListener('click', checkAnswer);
   });
 
-  getSpan.innerText = answer.style.backgroundColor;
+  getSpan.innerText = correct.style.backgroundColor;
 }
 
 generateColors();
 
 function checkAnswer({ target }) {
-  const instruction = document.getElementById('instruction');
+  const selected = document.querySelector('.selected')
+  if (selected) { selected.classList.remove('selected')}
+  const instruction = document.getElementById('answer');
   target.classList.add('selected');
-  (target.id === 'answer') ? instruction.innerText = 'Acertou! Novas cores!' : instruction.innerText = 'Errou! Tente novamente'
+  (target.id === 'correct') ? instruction.innerText = 'Acertou! Novas cores!' : instruction.innerText = 'Errou! Tente novamente'
 }
+
+const reset = document.getElementById('reset-game');
+reset.addEventListener('click', generateColors);
