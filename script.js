7ce180aaae3833da7c4cc698f1e2e28b4fd641e1
fabiana <fabianamfranco@outlook.com>
2021-09-30 14:12:41 -0300

    redescobrindo a quantas anda esse projeto

---
+++ script.js
@@ -0,0 +1,31 @@
+const balls = document.getElementsByClassName('ball');
+const paragrafo = document.getElementById('rgb-color');
+// const resposta = document.getElementById('answer');
+
+function gerandoRGB() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+paragrafo.innerText = gerandoRGB();
+
+const randomIndex = Math.floor(Math.random() * 7);
+
+for (let index = 0; index < balls.length; index += 1) {
+  if (index === randomIndex) {
+    balls[index].style.backgroundColor = paragrafo.innerText;
+  } else {
+    const corAleatoria = gerandoRGB();
+    balls[index].style.backgroundColor = corAleatoria;
+  }
+  // balls[index].addEventListener('click', (event) => {
+  // if (event.target.balls.style.backgroundColor === paragrafo) {
+  //   resposta.innerText = 'Acertou!'
+  // } else {
+  //   resposta.innerText = 'Errou! Tente novamente'
+  // }
+  // })
+}
